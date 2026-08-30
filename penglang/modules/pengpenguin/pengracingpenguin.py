6bedb0859f2b6cc58a3c49f0fdb77896ec5a0e1c
bebeday10 <185036455+bebeday10@users.noreply.github.com>
2026-08-30 12:42:24 +0800

    pyumpyumpyum

--- penglang/modules/pengpenguin/pengracingpenguin.py
+++ penglang/modules/pengpenguin/pengracingpenguin.py
@@ -1,12 +1,20 @@
+from dataclasses import dataclass
+
 from .pengpenguin import Penguin
 from ... import penglang as pl
 from .. import pengrandom as pr
 
+@dataclass(kw_only=True)
 class RacingPenguin(Penguin):
     power: int
     wit: int
     stamina: int
     guts: int
+    skills: None | list = None
+
+    def __post_init__(self):
+        self.skills = self.skills or []
+        return super().__post_init__()
 
     def speed_training(self):
         self.speed += pr.random_number(5, 15)
@@ -27,4 +35,10 @@ class RacingPenguin(Penguin):
     def wit_training(self):
         self.wit += pr.random_number(5, 15)
         self.power += pr.random_number(3, 5)
-        self.speed += pr.random_number(2, 4)
+        self.speed += pr.random_number(2, 4)\
+
+    def learn_skills(self, skill_type):
+        self.skills.append(skill_type)
+
+    def __str__(self):
+        return self.name
