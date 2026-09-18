82d177a50b07027e3c3ea7bc5bb130a10c107a1c
bebeday10 <185036455+bebeday10@users.noreply.github.com>
2026-09-18 15:19:13 +0800

    bundles

---
+++ penglang/karaoke.py
@@ -0,0 +1,10 @@
+"""
+The karaoke bundle.
+# Includes:
+    PenguinKaraokeMachine
+    PenguinSong
+    PengSongDesign
+"""
+from .modules.pengkaraoke import PenguinKaraokeMachine
+from .modules.pengsong import PenguinSong
+from .modules.pengsong.pengsongdesign import merge, convert_to_lyrics
---
+++ penglang/say.py
@@ -0,0 +1,20 @@
+from .penglang import (
+    say,
+    say_in_a_box,
+    dance,
+    shout,
+    penguin_noise,
+    penguin_ask
+)
+from .modules.pengprint import (
+    info_box,
+    emergency_box,
+    warning_box,
+    typewrite,
+    multitask_typewrite,
+    get_ice_size,
+    penguin_speech_bubble,
+    better_say,
+    better_say_in_a_box
+)
+from .modules.pengmenu import PenguinMenu
